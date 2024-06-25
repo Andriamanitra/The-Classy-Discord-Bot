c7be3108e499aa19626891288c199bf9ac4dbe50
Andriamanitra <mikko77792@gmail.com>
2024-06-26 00:23:53 +0300

    rip out image generation part

--- classy_bot/__main__.py
+++ classy_bot/__main__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-__all__ = ("ImagineInferenceSteps", "Bot")
+__all__ = ("Bot",)
 
 import os
 import json
@@ -14,78 +14,21 @@ from discord.ext import commands
 
 from . import utils
 from .quiz import QuizView, Quiz
-from .image_generation import ImageGenerator, NSFWImageGenerationError
 from . import codeguessr
 
 
 dotenv.load_dotenv(utils.resolve_relative_path(__file__, "../dotenv/.env"))
 
 
-ImagineInferenceSteps: Final = app_commands.transformers.RangeTransformer(
-    discord.enums.AppCommandOptionType.integer,
-    min = 5,
-    max = 500
-)
-
-
 class Cog(commands.Cog):
     def __init__(self) -> None:
         super().__init__()
-        self.image_generator = ImageGenerator()
-        
+
     @app_commands.command(
         description="Hello!"
     )
     async def hello(self, interaction: discord.Interaction) -> None:
         await interaction.response.send_message('Hello')
-
-    default_imagine_inference_steps: Final = 50
-    @app_commands.command(
-        description="Turns your prompts into art"
-    )
-    @app_commands.describe(
-        prompt="Provide a description of the image you want to create",
-        negative_prompt="Describe elements you want to avoid in the image",
-        inference_steps=
-            "The more steps, the higher the quality "
-            f"(min: {ImagineInferenceSteps.min_value}, "
-            f"max: {ImagineInferenceSteps.max_value}, "
-            f"default: {default_imagine_inference_steps})"
-    )
-    async def imagine(
-        self,
-        interaction: discord.Interaction,
-        prompt: str,
-        negative_prompt: str = "",
-        inference_steps: ImagineInferenceSteps = default_imagine_inference_steps # type: ignore
-    ) -> None:
-        
-        await interaction.response.defer(thinking = True)
-
-        try:
-            image_url = await self.image_generator.generate(
-                prompt=prompt,
-                negative_prompt=negative_prompt,
-                width=1216,
-                height=832,
-                num_inference_steps=inference_steps
-            )
-            
-            embed = discord.Embed(
-                title=f"\\> {prompt[:125]+'...' if len(prompt) > 128 else prompt}",
-                description=f"by {interaction.user.mention}",
-                color=discord.Color.yellow()
-            )
-            embed.set_image(url=image_url)
-            
-            await interaction.followup.send(embed=embed)
-
-        except NSFWImageGenerationError:
-            await interaction.followup.send("**NSFW content detected**")
-
-        except Exception as exception:
-            await interaction.followup.send("**Something went wrong**")
-            raise exception
 
     @app_commands.command(
         description="Solve a short math question"
